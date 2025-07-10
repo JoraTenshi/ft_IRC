6dcd60328ec7331408b1d6e8c36cf22537e75e51
Jorge Callejo Martinez <jcallejo@c3r3s4.42malaga.com>
2025-07-10 15:18:02 +0200

    forgor to add else if to check is pass has been filled sowwy

--- src/Commands/ExecuteCommand.cpp
+++ src/Commands/ExecuteCommand.cpp
@@ -11,6 +11,8 @@ void Server:: executeCommand(User &user)
 
 	if (user.getMessage().getCmd() == "PASS")
 		PassCmd(user);
+	else if (!user.getPassword())
+		std::cout << " [ SERVER ] Client " << user.getFd() << " has not sent correct password. Denied." << std::endl;
 	else if (user.getMessage().getCmd() == "NICK")
 		NickCmd(user);
 	else if (user.getMessage().getCmd() == "USER")
